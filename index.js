1ad56ca492781672bbb6b2c3335839a8fe13ae30
kizdolf <jules.buret@gmail.com>
2016-02-29 13:11:54 +0100

    login working. Need refacto, need better logic as well.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
-	This is nothing but a draft. 
+	This is nothing but a draft.
 	More tests than code.
 	So just don't use it for now :)
 */
@@ -8,127 +8,137 @@
 var request = require('request');
 var crypto = require('crypto');
 
-var apiUrl, apiUid, appToken, appId, granted;
-
-/*HELPERS*/
-var stripTrailingSlash = (str) => {
-    if(str.substr(-1) === '/')
-        return str.substr(0, str.length - 1);
-    return str;
-}
-
-
-/*END HELPERS*/
-
-
-//url: the main url of your freebox (usually something like 12.123.42.56:34033)
-var init = (url, apiUid, appToken, appId, granted)=>{
-	apiUid =  apiUid; appToken =  appToken; appId =  appId; granted =  granted; //stupid stuff, I just needed to put all thoses vars in a git ignored file..
-	apiUrl = 'http://' + stripTrailingSlash(url);
-	//get version api, and check given url is right.
-	return new Promise((ful, rej)=>{
-		request(apiUrl + '/api_version', (err, res, body)=>{
-			if(err){
-				console.log(err);
-				rej(err);
-			}else{
-				var intel = JSON.parse(body);
-				apiUid = intel.uid;
-				apiUrl = apiUrl + intel.api_base_url + 'v' + intel.api_version.charAt(0);
-				ful(intel);
-			}
-		});
-	});
+var box = {
+    ip: '',
+    url: '',
+    token : '',
+    challenge: '',
+    status: '',
+    uid: '',
+    version: '',
+    base: ''
 };
 
+var app = {
+    token: '',
+    version: '',
+    name: '',
+    id: '',
+    device: '',
+    track: '',
+    password: ''
+};
 
-/**
-app = {
-	app_id: 'string', !mandatory
-	app_name: 'string', !mandatory
-	app_version: 'string' default: 0.4.2
-	}
-**/
-var authorize = (app, device)=>{
-	var postForm = {
-		'app_id': app.app_id,
-		'app_name': app.app_name,
-		'app_version': (app.app_version || '0.4.2'),
-		'device_name': device
+var getBox = (id, name, version, device, ip, cb)=>{
+    app.id = id;
+    app.name = name;
+    app.version = version;
+    app.device = device;
+    box.ip = ip;
+    box.url = 'http://' + box.ip;
+
+    var endpoint = box.url + '/api_version';
+    request(endpoint, (err, res, body)=>{
+        if(!err){
+            body = JSON.parse(body);
+            box.version = body.api_version.charAt(0);
+            box.uid = body.uid;
+            box.base = body.api_base_url;
+            box.url = box.url + box.base + 'v' + box.version + '/';
+            cb(body);
+        }
+    });
+};
+
+var isAuthorized = (track, cb)=>{
+    app.track = track;
+    var endpoint = box.url + 'login/authorize/' + app.track;
+    request(endpoint, (err, res, body)=>{
+        body = JSON.parse(body);
+        box.status = body.result.status;
+        box.challenge = body.result.challenge;
+        if(box.status == 'granted') cb(true);
+        else cb(false);
+    });
+};
+var authorize = (cb)=>{
+    var endpoint = box.url + 'login/authorize';
+    var options = {
+		url    : endpoint,
+		method : 'POST',
+		json   : {
+			   "app_id"      : app.id,
+			   "app_name"    : app.name,
+			   "app_version" : app.version,
+			   "device_name" : app.device
+			},
+		encode : 'utf-8'
 	};
-	appId = app.app_id;
-	console.log(postForm);
-	var urlPost = apiUrl + '/login/authorize';
-	console.log(urlPost);
-	return new Promise((ful, rej)=>{
-		request.post({url: urlPost, form: JSON.stringify(postForm)}, (err, res, body)=>{
-			if(err){
-				console.log(err);
-				rej(err);
-			}else{
-				var intel = JSON.parse(body);
-				ful(intel);
-			}
-		});
-	});
+    request(options, (err, res, body)=>{
+        if(!err){
+            // body = JSON.parse(body);
+            app.token = body.result.app_token;
+			app.track = body.result.track_id;
+            cb(app);
+        }
+    });
 };
 
-var authorized = (track_id)=>{
-	var url = apiUrl + '/login/authorize/' + track_id;
-	return new Promise((ful, rej)=>{
-		request(url, (err, res, body)=>{
-			if(err){
-				rej(err);
-			}else{
-				var intel = JSON.parse(body);
-				if(intel.result.status && intel.result.status == 'granted')
-					granted = true;
-				ful(intel);
-			}
-		});
-	});
+var getChalllenge = (cb)=>{
+    var endpoint = box.url + 'login';
+    request(endpoint, (err, res, body)=>{
+        body = JSON.parse(body);
+        box.challenge = body.result.challenge;
+        cb();
+    });
 };
 
-var getSessionToken = (app_token, challenge)=>{
-	challenge = challenge.replace(/\\/g, '');
-	app_token = app_token.replace(/\\/g, '');
-	console.log('getSessionToken');
-	console.log(challenge);
-	console.log(app_token);
-	var hash = crypto.createHmac('sha1', app_token).update(challenge).digest('hex');
-	console.log(hash);
-	return new Promise((ful, rej)=>{
-		request.post({url: apiUrl + '/login/session/', form: JSON.stringify({'app_id': appId, 'password': hash})}, (err, res, body)=>{
-			if(err){
-				rej(err);
-			}else{
-				var intel = JSON.parse(body);
-				ful(intel);
-			}
-		});
-	});
-}
+var login = (token, cb)=>{
+    getChalllenge(()=>{
+        var endpoint = box.url + 'login/session';
+        app.token = token;
+        app.password = crypto.createHmac('sha1', app.token).update(box.challenge).digest('hex');
+        var options = {
+            url    : endpoint,
+    		method : 'POST',
+    		json   : {
+    			   "app_id" : app.id,
+    			   "password" : app.password
+    			},
+    		encode : 'utf-8'
+        };
+        request(options, (err, res, body)=>{
+            if(!err){
+                if(body.success){
+                    box.token = body.result.session_token;
+                    cb(true);
+                }else cb(false);
+            }
+        });
+    });
+};
 
-var login = (app_token)=>{
-	var urlChallenge = apiUrl + '/login';
-	request(urlChallenge, (err, res, body)=>{
-		if(err){
-			console.log(err);
-		}else{
-			var intel = JSON.parse(body);
-			if(!intel.result.logged_in){
-				var challenge = intel.result.challenge;
-				getSessionToken(app_token, challenge).then((intel)=>{
-					console.log(intel);
-				});
-			}
-		}
-	});
+var lsFiles = (path, cb)=>{
+    path = new Buffer(path).toString('base64');
+    var endpoint = box.url + 'fs/ls/' + path;
+    var options = {
+        url : endpoint,
+        headers : { 'X-Fbx-App-Auth' : box.token },
+        method : 'GET',
+        encode: 'utf-8'
+	};
+    request(options, (err, res, body)=>{
+        if(!err){
+            body = JSON.parse(body);
+            cb(body.result);
+        }
+    });
 };
 
 module.exports = {
-	init: init,
-	authorize: authorize,
-	authorized: authorized,
-	login: login
+    getBox: getBox,
+    authorize: authorize,
+    isAuthorized: isAuthorized,
+    login: login,
+    lsFiles: lsFiles
 };
